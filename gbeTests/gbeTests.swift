7d22554a9299d9c6ff12173da4825abc1f977f8b
Aaron Bonham <abonham@switch.tv>
2018-08-21 09:50:45 +1000

    add example performance test

--- gbeTests/gbeTests.swift
+++ gbeTests/gbeTests.swift
@@ -58,7 +58,8 @@ class gbeTests: XCTestCase {
   func testPerformanceExample() {
     // This is an example of a performance test case.
     self.measure {
-      // Put the code you want to measure the time of here.
+      testCpu.processNext()
+      testCpu.pc.value = 0
     }
   }
   
